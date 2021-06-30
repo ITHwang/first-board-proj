609885c490de14f266856efab4ae68e11a9c55bd
BIG03 <BIG03@BIG03>
2021-06-30 16:09:41 +0900

    ADD func: view board

---
+++ first-board-proj/src/main/java/board/ArticleVO.java
@@ -0,0 +1,90 @@
+package board;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.sql.Date;
+
+public class ArticleVO {
+	private int articleNO;
+	private String title;
+	private String content;
+	private String imageFileName;
+	private Date writeDate;
+	private String id;
+
+	public ArticleVO() {
+	}
+
+	public ArticleVO(int articleNO, String title, String content, String imageFileName, String id, Date writeDate) {
+		this.articleNO = articleNO;
+		this.title = title;
+		this.content = content;
+		this.imageFileName = imageFileName;
+		this.id = id;
+		this.writeDate = writeDate;
+	}
+
+	public int getArticleNO() {
+		return articleNO;
+	}
+
+	public void setArticleNO(int articleNO) {
+		this.articleNO = articleNO;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImageFileName() {
+		try {
+			if (imageFileName != null && imageFileName.length() != 0) {
+				imageFileName = URLDecoder.decode(imageFileName, "utf-8");
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return imageFileName;
+	}
+
+	public void setImageFileName(String imageFileName) {
+		try {
+			if (imageFileName != null && imageFileName.length() != 0) {
+				this.imageFileName = URLEncoder.encode(imageFileName, "utf-8");
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getWriteDate() {
+		return writeDate;
+	}
+
+	public void setWriteDate(Date writeDate) {
+		this.writeDate = writeDate;
+	}
+
+}
